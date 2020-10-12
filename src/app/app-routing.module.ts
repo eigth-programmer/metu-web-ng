c62034f5db51617ccd8f9b6804b6e9d1e43ed992
eigth-programmer <oigresanep@gmail.com>
2020-10-12 21:13:23 +0200

    master
    * Basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './shared/presentation/home/home.component';
+import { LoginComponent } from './auth/presentation/login/login.component';
+import { RegisterComponent } from './auth/presentation/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'store',
+    loadChildren: () => import('./store/store.module').then(mod => mod.StoreModule)
+  },
+  {
+    path: 'coffee-shop',
+    loadChildren: () => import('./coffeeshop/coffeeshop.module').then(mod => mod.CoffeeshopModule)
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
