296457627104f223107ae05a0ad224ca741d3385
sergio.pena <sergio.pena@bosonit.com>
2020-12-06 12:27:45 +0100

    master
    * Added config file

---
+++ src/app/store/config/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  email: 'oigresanep@gamil.com',
+  phone: '637209358',
+  address: 'Calle de la piruleta 24',
+  linkedin: 'https://www.linkedin.com/in/sergio-pe%C3%B1a-bayona-297308128/',
+  facebook: '',
+  instagram: '',
+  aboutUsShort: 'resume',
+};
